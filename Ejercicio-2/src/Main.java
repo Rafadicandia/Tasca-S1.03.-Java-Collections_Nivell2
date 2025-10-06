2647bb45582e486f58054df2df420ca53ec913e0
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-06 13:15:29 +0200

    Copiaos las clases del ejercicio y agregamos la interzas comparable

---
+++ Ejercicio-2/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HashSet <Restaurant> newRestaurantList= new HashSet<>();
+
+        Restaurant pepito = new Restaurant("Pepito", 5);
+        Restaurant manolito = new Restaurant("Manolito", 3);
+        Restaurant pepito2 = new Restaurant("Pepito", 3);
+        Restaurant chiringuito = new Restaurant("El chiringuito de Carlitos", 1);
+        Restaurant pepito3 = new Restaurant("Pepito", 5);
+
+
+        newRestaurantList.add(pepito);
+        newRestaurantList.add(pepito2);
+        newRestaurantList.add(manolito);
+        newRestaurantList.add(chiringuito);
+
+
+
+        System.out.println("The list of total restaurants is: "+newRestaurantList.size());
+
+        for (Restaurant r : newRestaurantList) {
+            System.out.println(r.getName()+"-"+"Puntuation: "+r.getPunctuation());
+        }
+
+
+
+    }
+}
---
+++ Ejercicio-2/src/Restaurant.java
@@ -0,0 +1,60 @@
+import java.util.Objects;
+
+public class Restaurant implements Comparable<Restaurant> {
+    private String name;
+    private  int punctuation;
+
+    public Restaurant (String name, int punctuation){
+        this.name=name;
+        this.punctuation=punctuation;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPunctuation(){
+        return punctuation;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // 1. Comprobación de la misma referencia: si es el mismo objeto en memoria
+        if (this == obj) {
+            return true;
+        }
+
+        // 2. Comprobación de nulidad y tipo: si el objeto es null o no es de la misma clase
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        // 3. Conversión (Casting): convertimos el objeto genérico a nuestro tipo
+        Restaurant other = (Restaurant) obj;
+
+        // 4. Lógica de Negocio: la comparación de atributos (tu lógica)
+        return this.punctuation == other.punctuation && this.name.equals(other.name);
+    }
+
+    @Override
+    public int hashCode() {
+        // La regla del contrato: usar los MISMOS atributos que en equals()
+        return Objects.hash(name, punctuation);
+    }
+
+    @Override
+    public int compareTo(Restaurant other) {
+        // 1. Comparamos los nombres
+        int comparacionNombre = this.name.compareTo(other.name);
+
+        // 2. Si los nombres son diferentes (comparacionNombre != 0), devolvemos ese resultado.
+        if (comparacionNombre != 0) {
+            return comparacionNombre;
+        }
+
+        return other.punctuation - this.punctuation;
+    }
+
+
+
+}
