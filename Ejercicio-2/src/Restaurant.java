70fa0cade6ee49ffacd1ae74f67f108364deae91
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2025-10-08 12:11:45 +0200

    refactor: reomve unnecesary lines

--- Ejercicio-2/src/Restaurant.java
+++ Ejercicio-2/src/Restaurant.java
@@ -29,35 +29,35 @@ public class Restaurant implements Comparable<Restaurant> {
 
     @Override
     public boolean equals(Object obj) {
-        // 1. Comprobación de la misma referencia: si es el mismo objeto en memoria
+
         if (this == obj) {
             return true;
         }
 
-        // 2. Comprobación de nulidad y tipo: si el objeto es null o no es de la misma clase
+
         if (obj == null || getClass() != obj.getClass()) {
             return false;
         }
 
-        // 3. Conversión (Casting): convertimos el objeto genérico a nuestro tipo
+
         Restaurant other = (Restaurant) obj;
 
-        // 4. Lógica de Negocio: la comparación de atributos (tu lógica)
+
         return this.punctuation == other.punctuation && this.name.equals(other.name);
     }
 
     @Override
     public int hashCode() {
-        // La regla del contrato: usar los MISMOS atributos que en equals()
+
         return Objects.hash(name, punctuation);
     }
 
     @Override
     public int compareTo(Restaurant other) {
-        // 1. Comparamos los nombres
+
         int comparacionNombre = this.name.compareTo(other.name);
 
-        // 2. Si los nombres son diferentes (comparacionNombre != 0), devolvemos ese resultado.
+       
         if (comparacionNombre != 0) {
             return comparacionNombre;
         }
